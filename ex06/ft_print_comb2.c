4c7c9ed1210c9bcf34cf9f3b5182d282bdc269bb
Your Name <lmaujean@c2r4p4.42nice.fr>
2021-01-18 09:36:00 +0100

    coucou

---
+++ ex06/ft_print_comb2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmaujean <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/15 09:56:37 by lmaujean          #+#    #+#             */
+/*   Updated: 2021/01/15 09:56:41 by lmaujean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a < 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if (b == 99 && a == 98)
+			{
+				ft_putchar('\n');
+				break ;
+			}
+			ft_putchar(',');
+			ft_putchar(' ');
+			b++;
+		}
+		a++;
+	}
+}
